c3d97660b69eed40872eab6910133116df61a9b8
ullah <sheikholeei97@gmail.ocm>
2022-11-08 20:09:34 +0600

    firebase env variable working properly

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,37 +1,12 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_apiKey,
-//   authDomain: process.env.REACT_APP_authDomain,
-//   projectId: process.env.REACT_APP_projectId,
-//   storageBucket: process.env.REACT_APP_storageBucket,
-//   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//   appId: process.env.REACT_APP_appId
-// };
-
-// // Initialize Firebase
-// export const app = initializeApp(firebaseConfig);
-
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA5aUQTp-yOK-Tm4abfCRg6Bzt6gpjEupw",
-  authDomain: "dencure-smilefresh.firebaseapp.com",
-  projectId: "dencure-smilefresh",
-  storageBucket: "dencure-smilefresh.appspot.com",
-  messagingSenderId: "153148387914",
-  appId: "1:153148387914:web:e66aef84e5c40672e12cb3"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
