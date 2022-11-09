f395f4625e6e7d5ab074a1f63711982b2ca9e4f4
ullah <sheikholeei97@gmail.ocm>
2022-11-09 21:26:49 +0600

    review page done

--- src/pages/MyReview.js
+++ src/pages/MyReview.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { useState } from 'react';
 import { useContext } from 'react';
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Ratings from '../components/Others/Ratings';
 import { AuthContext } from '../contexts/AuthProvider';
 
 const MyReview = () => {
   const {user,setLoading} = useContext(AuthContext);
   const [reviews, setReviews] = useState([]);
-
 
 
     useEffect(() => {
@@ -26,14 +27,38 @@ const MyReview = () => {
 
 
   return (
-    <div>
-      {
-        reviews ?
-        reviews.map(review => <p>kemonachiljadfjl</p>)
-        :
-        <div>There have an error</div>
-      }
+    <div className='container mx-auto md:px-10 py-14 px-6'>
+      {reviews.length> 0 && <h1 className='text-center md:text-4xl text-2xl font-bold pb-8 capitalize text-gray-600'>My reviews On dencure</h1>}
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
+            {
+
+                reviews.length > 0 ?
+                reviews.map(({_id,review, heading,author,message,avatar}) => (
+                    <div className='text-center p-4 shadow-lg text-black'>
+                        <div className='w-[200px] mx-auto block pl-12 mb-4'>
+                            <Ratings rating={review} />
+                        </div>
+                        <h3 className='text-xl font-bold uppercase mb-3'>{heading}</h3>
+                        <p className='text-sm text-gray-700 mb-4'>{message}</p>
+                        <div className='w-[60px] h-[60px] mx-auto rounded-full'>
+                            <img src={avatar} alt="" className='rounded-full aspect-square' />
+                        </div>
+                        <p className='py-3 text-xl font-bold'>{author}</p>
+
+                        <div className='flex items-center justify-center gap-3'>
+                          <button className='btn btn-sm btn-primary block'>Update</button>
+                          <button className='btn btn-sm btn-secondary block'>Delete</button>
+                        </div>
+                    </div>
+                )) :
+                <div className='md:col-span-2 lg:col-span-3 flex flex-col items-center justify-center h-[60vh] space-y-6'>
+                    <p className='text-2xl md:text-4xl text-blue-600 font-bold'>You have no review to display!!!</p>
+                    <Link to={'/services'}><button className='btn btn-sm btn-primary'>Please Add review !!!</button></Link>
+                </div>
+            }
+        </div>
     </div>
+
   )
 }
 
