41567a5fff769e51ea3350c62ed24812bdf90aa7
ullah <sheikholeei97@gmail.ocm>
2022-11-10 09:15:15 +0600

    update review successfully done

--- src/pages/UpdateReview.js
+++ src/pages/UpdateReview.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useState } from 'react';
 import { useContext } from 'react';
+import { useEffect } from 'react';
 import toast from 'react-hot-toast';
 import { Link, useParams } from 'react-router-dom';
 import updatePhoto from '../assets/images/update.png'
@@ -8,76 +8,88 @@ import { AuthContext } from '../contexts/AuthProvider';
 import useTitle from '../hooks/useTitle';
 
 const UpadateReview = () => {
-    useTitle('Add Review')
-    const {user} = useContext(AuthContext);
-    const {id} = useParams();
+    const {setLoading} = useContext(AuthContext);
+    const [review, setReview] = useState({});
+    useTitle('Update Review')
+    const { id } = useParams();
 
 
-    //get all the data form mongodb------>
-    // useEffect(() => {
-    //     fetch(`http://localhost:4000/getreview/${id}`)
-    //     .then(res => res.json())
-    //     .then()
-    // },[])
+    // get all the data form mongodb------>
+    useEffect(() => {
+        fetch(`http://localhost:4000/getreview/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                setReview(data);
+            })
+            .catch(err => console.log(err.message))
+    }, [id, setLoading])
 
-// const handleSubmit = (e) => {
-//     e.preventDefault();
-//     const form = e.target;
-//     const message = form.message.value;
-//     const name = form.name.value;
-//     const rating = form.rating.value;
-//     const brief = form.brief.value;
-//     form.reset();
 
-//     const sendingData = {id,name,message,rating,brief, email: user?.email, img: user?.photoURL || 'https://cdn.icon-icons.com/icons2/2643/PNG/512/male_boy_person_people_avatar_icon_159358.png'};
+    //pathcing data to update the review in mongodb.
+    const updateReview = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const message = form.message.value;
+        const author = form.name.value;
+        const review = form.rating.value;
+        const heading = form.brief.value;
+        const avatar = form.avatar.value;
+        form.reset();
 
-//     fetch(`http://localhost:4000/addreview/${id}`, {
-//         method: 'POST',
-//         headers: {
-//             'content-type': 'application/json'
-//         },
-//         body: JSON.stringify(sendingData)
-//     })
-//     .then(res => {
-//         return res.json()
-//     })
-//     .then(data => {
-//         if(data.status === 'successful'){
-//             toast.success('Successfully Added your review !!!')
-//         }
-//     })
-// };
+        const sendingData = {author, review, message, heading, avatar};
 
-    return (<div className='flex justify-center items-center md:h-screen bg-gray-100'>
+        fetch(`http://localhost:4000/updatereview/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(sendingData)
+        })
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                if (data.modifiedCount === 1) {
+                    toast.success('Successfully updated your review !!!')
+                }
+            })
+    };
+
+    return (<div className='flex justify-center items-center md:h-auto bg-gray-100'>
         <div className='grid md:grid-cols-2 md:px-6'>
             <div>
                 <img src={updatePhoto} alt="registerPhoto" />
             </div>
             <div className='p-5'>
-                <form className='space-y-4 border border-gray-300 md:w-2/3 mx-auto p-5 rounded'>
-                    <h3 className='text-2xl font-bold text-gray-500'>Your Valuable Reviw</h3>
+                <form onSubmit={updateReview} className='space-y-4 border border-gray-300 md:w-2/3 mx-auto p-5 rounded'>
+                    <h3 className='text-2xl font-bold text-gray-500'>Update Review</h3>
                     <div className='flex flex-col'>
                         <label htmlFor="Name" className='text-gray-400 font-semibold'>Author Name</label>
-                        <input type="text" name='name' className='bg-white border border-gray-400 py-1 px-3 text-gray-800 focus:outline-none rounded' required />
+                        <input type="text" name='name' className='bg-white border border-gray-400 py-1 px-3 text-gray-800 focus:outline-none rounded' required defaultValue={review?.author} />
                     </div>
+
 
                     <div className='flex flex-col'>
                         <label htmlFor="Name" className='text-gray-400 font-semibold'>Short Review</label>
-                        <textarea type="text" name='brief' className='bg-white border border-gray-400 py-1 px-3 text-gray-800 focus:outline-none rounded' required />
+                        <textarea type="text" name='brief' className='bg-white border border-gray-400 py-1 px-3 text-gray-800 focus:outline-none rounded' required defaultValue={review?.heading} />
                     </div>
 
+                    <div className='flex flex-col'>
+                        <label htmlFor="Name" className='text-gray-400 font-semibold'>Author image Link</label>
+                        <textarea type="text" rows={3} name='avatar' className='bg-white border border-gray-400 py-1 px-3 text-gray-800 focus:outline-none rounded' required defaultValue={review?.avatar} />
+                    </div>
 
                     <div className='flex flex-col'>
                         <label htmlFor="Name" className='text-gray-400 font-semibold'>Say something briefly</label>
-                        <textarea type="text" name='message' className='bg-white border border-gray-400 py-1 px-3 text-gray-800 focus:outline-none rounded' required />
+                        <textarea type="text" rows={5} cols={10} name='message' className='bg-white border border-gray-400 py-1 px-3 text-gray-800 focus:outline-none rounded' required defaultValue={review?.message} />
                     </div>
 
                     <div className='flex flex-col'>
                         <label htmlFor="Name" className='text-gray-400 font-semibold'>Ratings</label>
-                        <input type="number" max={'5'} min="0" name='rating' className='bg-white border border-gray-400 py-1 px-3 text-gray-800 focus:outline-none rounded' required />
+                        <input type="number" max={'5'} min="0" step='any' name='rating' className='bg-white border border-gray-400 py-1 px-3 text-gray-800 focus:outline-none rounded' required defaultValue={review?.review} />
                     </div>
 
-                    <input type='submit' value={'Review Us'} className='bg-[#0ed39e] w-full rounded py-2 font-semibold text-xl text-white hover:bg-[#09e5ab] duration-300' />
+                    <input type='submit' value={'Update Review'} className='bg-[#0ed39e] w-full rounded py-2 font-semibold text-xl text-white hover:bg-[#09e5ab] duration-300' />
 
                     <Link to={`/services/${id}`} className='text-blue-600 font-semibold py-2 block hover:text-[#09e5ab] text-right'>Back to Review page</Link>
                 </form>
